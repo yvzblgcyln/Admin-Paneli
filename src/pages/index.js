115cb1b5d5b45238babedf4d38887d1039173050
Yavuz Bilge Ceylan <103778738+yvzblgcyln@users.noreply.github.com>
2023-07-27 11:26:28 +0300

    small fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { t } from "i18next";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -7,9 +8,15 @@ export default function Home() {
       <Head>
         <title>{t("tab-home")}</title>
       </Head>
-      <h1 className="d-flex d-flex flex-column justify-content-center align-items-center" style={{ height: "100%" }}>
-        {t("welcome")}
-      </h1>
+      <div className="d-flex d-flex flex-column justify-content-center align-items-center" style={{ height: "100%" }}>
+        <h1>{t("welcome")}</h1>
+        <Link href="/create-company" style={{ color: "black" }}>
+          {t("create-company")}
+        </Link>
+        <Link href="/company-list" style={{ color: "black" }}>
+          {t("company-list")}
+        </Link>
+      </div>
     </>
   );
 }
